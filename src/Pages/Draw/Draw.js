5e51a4a78e051eabe347e31ee6d76071e0b5ad67
dawnheee <sherlock130815@gmail.com>
2022-09-09 22:00:27 +0900

    초기세팅

---
+++ src/Pages/Draw/Draw.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Draw() {
+  return <div>Draw</div>;
+}
+
+export default Draw;
---
+++ src/Pages/Find/FoundItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FoundItem() {
+  return <div>FoundItem</div>;
+}
+
+export default FoundItem;
